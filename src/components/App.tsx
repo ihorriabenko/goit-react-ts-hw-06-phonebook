bb1a83cd1444f80d5544175b4bc9acd1baaa23bf
Ihor Riabenko <100082550+iryabenkoff@users.noreply.github.com>
2023-09-08 16:48:03 +0300

    feature/Section
    Edit Title

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,19 @@
 import ContactsForm from './ContactsForm/ContactsForm';
 import ContactsList from './ContactsList/ContactsList';
 import Filter from './Filter/Filter';
-import Title from './Title/Title';
+import Section from './Section/Section';
 import s from './app.module.css';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <div className={s.app}>
-      <Title type='h2' text='Phonebook' />
-      <ContactsForm />
-      <Title type='h2' text='Contacts' />
-      <Filter />
-      <ContactsList />
+      <Section type='h2' text='Phonebook'>
+        <ContactsForm />
+      </Section>
+      <Section type='h2' text='Contacts'>
+        <Filter />
+        <ContactsList />
+      </Section>
     </div>
   );
 };
